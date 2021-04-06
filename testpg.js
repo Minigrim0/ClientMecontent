28efb82f1d57ad4ca0286dcfc64abebf2d8968ef
Minigrim0 <grimauflorent@gmail.com>
2021-04-06 20:01:58 +0200

    [Del] last JS bit

--- testpg.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg')
-const pgClient = new Client()
-
-;(async () => {
-  await pgClient.connect()
-  const res = await pgClient.query('SELECT $1::text as message', ['Hello world!'])
-  console.log(res.rows[0].message) // Hello world!
-  await pgClient.end()
-})()
