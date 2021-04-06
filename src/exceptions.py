8bded07ac9ba1aad15f88ef70ef1daa41f59080a
Minigrim0 <grimauflorent@gmail.com>
2021-04-06 22:35:44 +0200

    [Add] BadFormatException

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,4 +3,15 @@ class CommandNotFoundException(Exception):
         self.command = command
 
     def __str__(self):
-        return "the cpmmand {self.command} could not be found, type !help for help"
+        return f"the command '{self.command}' could not be found, type !help for help"
+
+
+class BadFormatException(Exception):
+    def __init__(self, command: str, pattern: str):
+        self.command = command
+        self.pattern = pattern
+
+    def __str__(self):
+        return (
+            f"the command '{self.command}' does not follow the pattern '{self.pattern}'"
+        )
