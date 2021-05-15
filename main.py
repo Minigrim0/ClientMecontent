6793ef3e0720c2592ecdce1b2834738f770ac75c
Minigrim0 <grimauflorent@gmail.com>
2021-05-15 18:14:54 +0200

    [Add] database loading in main

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import logging
 import os
 
 from singleton.client import Bot
+from singleton.database import Database
 
 if __name__ == "__main__":
     client = Bot.getInstance()
+    database = Database.getInstance()
 
     token = os.environ.get("API_KEY", None)
     if token is None:
