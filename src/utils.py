1f36a9d62d1532e7a2df47d0a9837c3789e636af
Minigrim0 <grimauflorent@gmail.com>
2021-05-15 16:54:21 +0200

    [Fix] send error message

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 async def send_error_message(decorator_kwargs, error_msg: str):
     if type(decorator_kwargs) is dict:
+        if "channel" in decorator_kwargs.keys():
         channel = decorator_kwargs["command"].channel
     else:
-        print(decorator_kwargs)
-        print(type(decorator_kwargs))
         channel = decorator_kwargs["args"]["channel"]
+    else:
+        return
 
     await channel.send(error_msg)
