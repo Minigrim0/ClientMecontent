af817f7f4247000af23bf77cbcdcc8836dcbbed9
Minigrim0 <grimauflorent@gmail.com>
2021-05-15 16:59:15 +0200

    [Add] database location finder

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,19 @@
+from src.settings import Settings
+
+
 async def send_error_message(decorator_kwargs, error_msg: str):
     if type(decorator_kwargs) is dict:
         if "channel" in decorator_kwargs.keys():
-        channel = decorator_kwargs["command"].channel
-    else:
-        channel = decorator_kwargs["args"]["channel"]
+            channel = decorator_kwargs["command"].channel
+        else:
+            channel = decorator_kwargs["args"]["channel"]
     else:
         return
 
     await channel.send(error_msg)
+
+
+def databaseLocation():
+    """Returns the location of the database file
+    """
+    return Settings.getInstance()["database"]["location"]
