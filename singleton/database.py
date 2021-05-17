e87074672b39d6a7b9a93227feac73fab8967532
Minigrim0 <grimauflorent@gmail.com>
2021-05-17 09:55:49 +0200

    [Upd] scripts are loaded

--- singleton/database.py
+++ singleton/database.py
@@ -12,14 +12,11 @@ class Database:
         return Database.instance
 
     @connected
-    def __init__(self, db, cursor):
+    def __init__(self, db, cursor, scripts):
         if Database.instance is not None:
             raise Exception("This class is a Singleton!")
         else:
             Database.instance = self
 
-        with open("assets/init_db.sql", "r") as sql_file:
-            sql_script = sql_file.read()
-
-        cursor.executescript(sql_script)
+        cursor.executescript(scripts["init_db"])
         db.commit()
