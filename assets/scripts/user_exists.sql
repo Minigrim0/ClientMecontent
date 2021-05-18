54d49780c402b95f9cf08b299e54deb7ec6d2355
Minigrim0 <grimauflorent@gmail.com>
2021-05-18 23:09:02 +0200

    [Add] user score script ans user exists

---
+++ assets/scripts/user_exists.sql
@@ -0,0 +1,12 @@
+-- Returns whether the user exists or not
+
+SELECT
+    CASE COUNT(*) 
+        WHEN 1
+            THEN true
+        ELSE false
+    END
+FROM
+    Users
+WHERE
+    discord_id = ?
---
+++ assets/scripts/user_score.sql
@@ -0,0 +1,2 @@
+-- Returns the score of the user with the given ID
+SELECT score FROM Users WHERE discord_id = ?
