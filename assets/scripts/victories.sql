1a0ee0666f0e7e16dad6d2533376cea19b1f71c9
Minigrim0 <grimauflorent@gmail.com>
2021-05-19 10:51:23 +0200

    [Upd] count winner only if the game is finished

--- assets/scripts/victories.sql
+++ assets/scripts/victories.sql
@@ -1,1 +1,21 @@
-SELECT COUNT(*) FROM (SELECT userToGame.user_id as winner_id FROM Game LEFT JOIN userToGame ON userToGame.game_id = Game.id AND userToGame.votes=(SELECT MAX(votes) FROM userToGame WHERE game_id=Game.id)) WHERE winner_id = ?
+SELECT
+    COUNT(*)
+FROM
+    (SELECT
+        userToGame.user_id as winner_id
+    FROM
+        Game
+    LEFT JOIN
+        userToGame ON userToGame.game_id = Game.id AND userToGame.votes=(
+        SELECT
+            MAX(votes)
+        FROM
+            userToGame
+        WHERE
+            game_id=Game.id
+        )
+    WHERE
+        Game.finished = true
+    )
+WHERE
+    winner_id = ?
