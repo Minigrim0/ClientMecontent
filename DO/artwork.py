ee6a562c80dd3250183438e2d4639206e8e2f65f
Minigrim0 <grimauflorent@gmail.com>
2021-06-29 16:42:14 +0200

    [Add] ArtworkDO

---
+++ DO/artwork.py
@@ -0,0 +1,5 @@
+class ArtworkDO:
+    def __init__(self, artwork_title: str, artwork_url: str, user_id):
+        self.user_id = user_id
+        self.artwork_title = artwork_title
+        self.artwork_url = artwork_url
