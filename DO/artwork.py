b3cfa6d53699c39c80a9d5852af87fd9e2754465
Minigrim0 <grimauflorent@gmail.com>
2021-07-03 22:03:52 +0200

    [Fix] user displaying

--- DO/artwork.py
+++ DO/artwork.py
@@ -4,7 +4,8 @@ from discord import Embed
 class ArtworkDO:
     """Represents the data of an artwork"""
 
-    def __init__(self, artwork_title: str, artwork_url: str, user_id):
+    def __init__(self, id: int, artwork_title: str, artwork_url: str, user_id):
+        self.id = id
         self.user_id = user_id
         self.artwork_title = artwork_title
         self.artwork_url = artwork_url
@@ -15,10 +16,8 @@ class ArtworkDO:
         em = Embed(title=f"Participation #{index} (Partie #{game_id})")
         em.add_field(name="Titre de l'œuvre", value=self.artwork_title)
 
-        print("Getting user :", self.user_id)
         user = await Bot.getInstance().fetch_user(int(self.user_id))
-        print("User = ", user)
-        author = user.username if revealAuthor else "XXXXXXXX"
+        author = user.name if revealAuthor else "XXXXXXXX"
 
         em.set_image(url=self.artwork_url)
         em.set_author(name=author)
