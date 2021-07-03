e3630eb040d9140868611df118ed18f3061ca8b4
Minigrim0 <grimauflorent@gmail.com>
2021-07-03 16:41:41 +0200

    [Fix] getArtwork script

--- assets/scripts/get_artworks.sql
+++ assets/scripts/get_artworks.sql
@@ -1,2 +1,2 @@
 -- Returns the artworks relative to the game with the given ID
-SELECT Artwork.title, Artwork.url, user_id FROM UserToGame LEFT JOIN Artwork ON artwork_id = Artwork.ID WHERE game_id = ?
+SELECT title, url, userToGame.user_id FROM Artwork LEFT JOIN userToGame on ID = userToGame.artwork_id WHERE userToGame.game_id = ?
