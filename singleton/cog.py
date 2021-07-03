56a79800c818911684da68b81cb95f2e93742320
Minigrim0 <grimauflorent@gmail.com>
2021-07-03 12:04:51 +0200

    [Add] cog singleton

---
+++ singleton/cog.py
@@ -0,0 +1,37 @@
+from discord.ext import commands, tasks
+
+
+class GameCog(commands.Cog):
+    instance = None
+
+    @staticmethod
+    def getInstance():
+        """Returns the instance of the singleton
+        Returns:
+            GameCog: The instance
+        """
+        if GameCog.instance is None:
+            GameCog()
+        return GameCog.instance
+
+    def __init__(self):
+        if GameCog.instance is not None:
+            raise Exception("This class is a singleton !")
+        GameCog.instance = self
+
+        self.nearGames = []
+        self.nearVotes = []
+
+    @tasks.loop(seconds=1)
+    async def endGameAndVotes(self):
+        for game in self.nearGames:
+            if game.justEnded():
+                await game.voteTime()
+
+        for vote in self.nearVotes:
+            if vote.justEnded():
+                await vote.showScore()
+
+    @tasks.loop(seconds=30)
+    def loadNearlyFinishedGamesAndVotes(self):
+        pass
